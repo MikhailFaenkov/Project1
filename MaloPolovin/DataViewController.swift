70d6732ee4af08b60a6f5fa40ae5735f6022d9ab
Programming <programming@macbook-pro-programming-6.local>
2019-01-12 15:36:41 +0300

    Initial Commit

---
+++ MaloPolovin/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  MaloPolovin
+//
+//  Created by Programming on 12.01.2019.
+//  Copyright © 2019 Programming. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
